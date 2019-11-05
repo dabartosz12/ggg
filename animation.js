38fbcbacec875d7531349cd1e576a2cdd872896e
dabill <42977352+dabartosz12@users.noreply.github.com>
2019-11-05 22:59:26 +0100

    new version

--- animation.js
+++ animation.js
@@ -2,28 +2,22 @@ const logo = document.querySelector(".logo");
 const menu = document.querySelector(".hamburger-menu");
 const imgHero = document.querySelector(".hero-img-start");
 const text = document.querySelector(".text-wrapper-in");
-const btnStart = document.querySelector(".explore-btn");
-const textSec = document.querySelector(".text-wrapper-in-sec");
-const Socials = document.querySelector(".socials-start");
+
 
 const tl = new TimelineMax();
 
 TweenMax.from(menu, 1, {
     opacity: "0", y: "-30px", 
     ease: Power2.easeInOut,
+    delay: 2,
 });
 
 TweenMax.from(logo, 1, {
     opacity: "0", y: "-30px", 
     ease: Power2.easeInOut,
+    delay: 2,
 });
 
-TweenMax.fromTo(imgHero, 2, {height: "0%"}, {height: "100%", ease: Expo.easeInOut, delay:.7})
+TweenMax.fromTo(imgHero, 2, {height: "0%"}, {height: "100%", ease: Expo.easeInOut, delay:.5})
 
-TweenMax.from(text, 1, {opacity:0, y: 20, delay:2, ease: Expo.easeInOut})
-
-TweenMax.from(textSec, 1, {opacity:0, y: 20, delay:2.5, ease: Expo.easeInOut})
-
-TweenMax.fromTo(btnStart, 1, {opacity: "0"}, {opacity: "1", delay: 3.5, ease: Power2.easeInOut})
-
-TweenMax.fromTo(Socials, 1, {opacity: "0"}, {opacity: "1", delay: 4.5, ease: Power2.easeInOut})
+TweenMax.from(text, 1, {opacity:0, y: 20, delay:2, ease: Expo.easeInOut, delay: 2.8})
--- menu.js
+++ menu.js
@@ -9,3 +9,22 @@ const menuXD = () => {
 }
 
 menuXD();
+
+//btn click go to top
+
+const bottomBtn = document.querySelector(".on-top-btn");
+let testVar = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    let testVar2 = window.pageYOffset;
+
+    if(testVar > testVar2) {
+        bottomBtn.style.display= "none";
+    }
+    else {
+        bottomBtn.style.display= "block";
+    }
+
+    testVar = testVar2;
+});
